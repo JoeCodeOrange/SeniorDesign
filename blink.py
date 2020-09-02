134e21e79e89ae622d2561c16df3195142128a31
unknown <codeorange.c@gmail.com>
2020-09-01 19:38:45 -0500

    Added a Stop and more blinks

--- blink.py
+++ blink.py
@@ -5,7 +5,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.output(4, True)
-time.sleep(1)
+time.sleep(5)
+GPIO.output(4, False)
+time.sleep(5)
+GPIO.output(4, True)
+time.sleep(5)
 GPIO.output(4, False)
 
 print("TEST")
