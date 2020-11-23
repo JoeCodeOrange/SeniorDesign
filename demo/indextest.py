83a97fc073ae98f1f3096712dba5a431da25aed7
Jose Elizondo <codeorange.c@gmail.com>
2020-11-23 15:57:01 -0600

    fuck it all motors test

--- demo/indextest.py
+++ demo/indextest.py
@@ -18,6 +18,10 @@ servo2 = GPIO.PWM(11,50) # Note 15 is pin, 50 = 50Hz pulse
 servo3 = GPIO.PWM(13,50) # Note 17 is pin, 50 = 50Hz pulse
 servo4 = GPIO.PWM(19,50) # Note 15 is pin, 50 = 50Hz pulse
 servo5 = GPIO.PWM(21,50) # Note 17 is pin, 50 = 50Hz pulse
+servo6 = GPIO.PWM(29,50) # Note 15 is pin, 50 = 50Hz pulse
+servo7 = GPIO.PWM(31,50) # Note 17 is pin, 50 = 50Hz pulse
+servo8 = GPIO.PWM(33,50) # Note 15 is pin, 50 = 50Hz pulse
+servo9 = GPIO.PWM(35,50) # Note 17 is pin, 50 = 50Hz pulse
 
 #start PWM running, but with value of 0 (pulse off)
 servo0.start(0)
